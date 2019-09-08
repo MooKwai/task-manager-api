2f506cbfb2896587692cf4f963fd37b868c0e99a
Sam <sjr.bkk@gmail.com>
2019-09-08 16:19:17 +0100

    Add POST route for creating a new user

--- src/index.js
+++ src/index.js
@@ -8,6 +8,15 @@ const port = process.env.port || 3000
 
 app.use(express.json())
 
+app.post('/users', (req, res) => {
+    const user = new User(req.body)
+    user.save()
+        .then(() => res.send(user))
+        .catch(error => {
+            res.status(400).send(error)
+        })
+})
+
 app.listen(port, () => {
     console.log('Server running on ' + port)
 })
