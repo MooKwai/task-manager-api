31181a5947e91fdc6536acee83ea69aa7b4a9ada
Sam <sjr.bkk@gmail.com>
2019-09-08 19:24:25 +0100

    Add DELETE route to delete a task

--- src/index.js
+++ src/index.js
@@ -146,6 +146,22 @@ app.patch('/tasks/:id', async (req, res) => {
     }
 })
 
+app.delete('/tasks/:id', async (req, res) => {
+    const id = req.params.id
+
+    try {
+        const task = await Task.findByIdAndDelete(id)
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
 app.listen(port, () => {
     console.log('Server running on ' + port)
 })
