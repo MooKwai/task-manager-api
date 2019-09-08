1a276bd452409a4f0271a6a2ee39a6e14ca84532
Sam <sjr.bkk@gmail.com>
2019-09-08 16:43:47 +0100

    Add GET route for reading one user

--- src/index.js
+++ src/index.js
@@ -21,6 +21,18 @@ app.get('/users', (req, res) => {
         .catch(error => res.status(500).send())
 })
 
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+    User.findById(_id)
+        .then(user => {
+            if (!user) {
+                return res.status(404).send()
+            }
+            res.send(user)
+        })
+        .catch(error => res.status(500).send(error))
+})
+
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
     task.save()
