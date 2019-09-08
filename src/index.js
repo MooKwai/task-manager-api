220bf04dc6d93ddb05d4671e8dcb72e68a0c9d03
Sam <sjr.bkk@gmail.com>
2019-09-08 16:28:03 +0100

    Update status for successes

--- src/index.js
+++ src/index.js
@@ -11,14 +11,14 @@ app.use(express.json())
 app.post('/users', (req, res) => {
     const user = new User(req.body)
     user.save()
-        .then(() => res.send(user))
+        .then(() => res.status(201).send(user))
         .catch(error => res.status(400).send(error))
 })
 
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
     task.save()
-        .then(() => res.send(task))
+        .then(() => res.status(201).send(task))
         .catch(error => res.status(400).send(error))
 })
 
