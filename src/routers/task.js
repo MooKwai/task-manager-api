bf19ce2e14853efa14263b7de816b3d4f4e66a36
Sam <sjr.bkk@gmail.com>
2019-09-10 06:02:27 +0100

    Authenticate get all tasks route

--- src/routers/task.js
+++ src/routers/task.js
@@ -17,10 +17,10 @@ router.post('/tasks', auth, async (req, res) => {
     }
 })
 
-router.get('/tasks', async (req, res) => {
+router.get('/tasks', auth, async (req, res) => {
     try {
-        const tasks = await Task.find({})
-        res.send(tasks)
+        await req.user.populate('tasks').execPopulate()
+        res.send(req.user.tasks)
     } catch (e) {
         res.status(500).send()
     }
@@ -30,7 +30,7 @@ router.get('/tasks/:id', auth, async (req, res) => {
     try {
         const task = await Task.findOne({
             _id: req.params.id,
-            owner: req.user.id
+            owner: req.user._id
         })
         if (!task) {
             return res.status(404).send()
